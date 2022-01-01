c9ca1d93e311aed5cabc9479589cb36aa694304b
Meharaz Hossain <59823440+meharaz2020@users.noreply.github.com>
2022-01-01 13:15:30 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,57 @@
+window.addEventListener("load",function(){
+    AOS.init();
+});
+
+//toggler
+const navToggler= document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", toggleNav);
+
+function toggleNav(){
+    navToggler.classList.toggle("active");
+    document.querySelector(".nav").classList.toggle("open");
+}
+
+//close when a click
+document.addEventListener("click",function(e){
+    if(e.target.closest(".nav-item")){
+        toggleNav();
+    }
+})
+
+//sticky
+
+window.addEventListener("scroll", function(){
+    if(this.pageYoffset > 60){
+        document.querySelector(".header").classList.add("sticky");
+    }else{
+        document.querySelector(".header").classList.remove("sticky");
+    }
+})
+
+
+
+
+//item(menu) button
+
+const menuTabs= document.querySelector(".menu-tabs");
+menuTabs.addEventListener("click" , function(e){
+    if(e.target.classList.contains("menu-tab-item") && !e.target.classList.contains("active")){
+        const target=e.target.getAttribute("data-target");
+        menuTabs.querySelector(".active").classList.remove("active");
+        e.target.classList.add("active");
+        const menuSection = document.querySelector(".menu-section");
+        menuSection.querySelector(".menu-tab-content.active").classList.remove("active");
+        menuSection.querySelector(target).classList.add("active");
+        AOS.init();
+    }
+});
+
+
+
+
+
+
+
+
+
+
